1e3d26fdb4e0bb6c3d213a427fbdf90bc2106f82
Starqx-ym <ccoriyadhira@gmail.com>
2025-10-03 16:11:33 -0500

    Paso 5: Creación de la Interfaz de Usuario

--- app/src/main/java/com/example/datossinmvvm/UserDao.kt
+++ app/src/main/java/com/example/datossinmvvm/UserDao.kt
@@ -2,6 +2,7 @@ package com.example.datossinmvvm
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -9,6 +10,9 @@ interface UserDao {
     @Query("SELECT * FROM User")
     suspend fun getAll(): List<User>
 
-    @Insert
+    @Query("SELECT * FROM User ORDER BY uid DESC LIMIT 1")
+    suspend fun getLastUser(): User?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(user: User)
 }
